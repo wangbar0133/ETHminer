be4f8b98fc61de8cd506c696baabce848216af14
wangbar0133 <53425789+wangbar0133@users.noreply.github.com>
2021-05-05 18:14:18 +0800

    Add files via upload

---
+++ 8_Ethereum-sparkpool.bat
@@ -0,0 +1,3 @@
+PhoenixMiner.exe -pool cn.sparkpool.com:3333 -wal sp_wangbar013 -worker rx5700 -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -coin eth
+pause
+
